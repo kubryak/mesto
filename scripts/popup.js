de3d3cac10f74d2f30e72fb0538b1638796c748e
Alexander Kubryak <al.lcn@yandex.ru>
2022-12-15 15:22:28 +0500

    fix: исправить ошибки после ревью

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,11 +3,14 @@ let popup = root.querySelector('.popup');
 let popupOpened = root.querySelector('.popupOpened');
 let buttonEditProfile = root.querySelector('.profile__edit-profile-info-btn');
 let buttonCloseEditProfile = root.querySelector('.popup__close-popup-btn')
-
+let profileName = root.querySelector('.profile__profile-name');
+let profileDescription = root.querySelector('.profile__profile-description');
 
 function popupEdit() {
     if (popup.classList.contains('popup_opened')) {
         popup.classList.remove('popup_opened');
+        popupName.value  = profileName.textContent;
+        popupDescription.value = profileDescription.textContent;
     } else {
         popup.classList.add('popup_opened');
     }
@@ -20,14 +23,13 @@ buttonCloseEditProfile.addEventListener('click', popupEdit);
 
 let popupName = root.querySelector('.popup__profile-name');
 let popupDescription = root.querySelector('.popup__profile-description');
-let profileName = root.querySelector('.profile__profile-name');
-let profileDescription = root.querySelector('.profile__profile-description');
 let buttonSaveProfile = root.querySelector('.popup__submit-profile-info-btn');
 
-function popupSave() {
+function popupSave(e) {
     profileName.textContent = `${popupName.value}`;
     profileDescription.textContent = `${popupDescription.value}`;
     popup.classList.add('popup_opened');
+    e.preventDefault();
 }
 
 popup.addEventListener('keydown', function (event) {
