31143b76906107fc8aca7eea2be0f2a7d4f7002f
Alexander Kubryak <al.lcn@yandex.ru>
2022-12-17 00:50:22 +0500

    fix: Исправить ошибки после ревью

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,19 +1,18 @@
 let root = document.querySelector('.root')
 let popup = root.querySelector('.popup');
-let popupOpened = root.querySelector('.popupOpened');
 let buttonEditProfile = root.querySelector('.profile__edit-profile-info-btn');
 let buttonCloseEditProfile = root.querySelector('.popup__close-popup-btn')
 let profileName = root.querySelector('.profile__profile-name');
 let profileDescription = root.querySelector('.profile__profile-description');
 
 function popupEdit() {
-    if (popup.classList.contains('popup_opened')) {
-        popup.classList.remove('popup_opened');
-        popupName.value  = profileName.textContent;
-        popupDescription.value = profileDescription.textContent;
-    } else {
-        popup.classList.add('popup_opened');
-    }
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened');
+    popupName.value = profileName.textContent;
+    popupDescription.value = profileDescription.textContent;
+  } else {
+    popup.classList.add('popup_opened');
+  }
 }
 
 buttonEditProfile.addEventListener('click', popupEdit);
@@ -26,18 +25,21 @@ let popupDescription = root.querySelector('.popup__profile_description');
 let buttonSaveProfile = root.querySelector('.popup__submit-profile-info-btn');
 
 function popupSave(e) {
-    profileName.textContent = `${popupName.value}`;
-    profileDescription.textContent = `${popupDescription.value}`;
-    popup.classList.add('popup_opened');
-    e.preventDefault();
+  e.preventDefault();
+  profileName.textContent = `${popupName.value}`;
+  profileDescription.textContent = `${popupDescription.value}`;
+  popupEdit();
+  if (e.keyCode === 13) {
+    popupEdit()
+  }
 }
 
-popup.addEventListener('keydown', function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        buttonSaveProfile.click();
-    }
-});
+// popup.addEventListener('keydown', function (event) {
+//   if (event.keyCode === 13) {
+//     event.preventDefault();
+//     popupEdit()
+//   }
+// });
 
 
-buttonSaveProfile.addEventListener('submit', popupSave)
+popup.addEventListener('submit', popupSave)
