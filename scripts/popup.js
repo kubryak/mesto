dd43b9b4c09cbe16a9ab16887fe5e1975671e522
kubryak <al.lcn@yandex.ru>
2023-01-27 23:17:49 +0500

    feat: Добавить валидацию всех форм страницы

--- scripts/popup.js
+++ scripts/popup.js
@@ -34,7 +34,18 @@ function openPopup(item) {
 // Закрытие попапов
 
 function closePopup(item) {
+  const inputs = item.querySelectorAll('.popup__input');
+  const inputsError = item.querySelectorAll('.popup__input-error');
+
   item.classList.remove('popup_opened');
+
+  inputs.forEach(element => {
+    element.classList.remove('popup__input_type_error');
+  })
+
+  inputsError.forEach(element => {
+    element.classList.remove('popup__input-error_active');
+  })
 };
 
 const buttonsClosePopup = root.querySelectorAll('.popup__close-btn');
@@ -195,4 +206,55 @@ popupFormMesto.addEventListener('submit', (e) => {
   e.target.reset();
 });
 
-//
+// ВАЛИДАЦИЯ ПОЛЕЙ
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+
+function setEventListeners (formElement) {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+
+  inputList.forEach(inputElement => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+
+function enableValidation () {
+  const formList = Array.from(document.querySelectorAll('.popup'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function(e){
+      e.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+
+
+enableValidation();
+
